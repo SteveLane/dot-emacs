5b27d03356b6d716b6ac642650c746661bd2b3ef
Steve Lane <lane.s@unimelb.edu.au>
2016-11-18 14:06:47 +1100

    Add colour to log for magit

--- init.el
+++ init.el
@@ -126,6 +126,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(magit-log-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
  '(magit-tag-arguments (quote ("--annotate")))
  '(package-selected-packages
    (quote
