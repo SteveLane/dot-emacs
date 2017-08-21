8b9b041fb21489c50892c21ae281aea1e0430cc1
Steve Lane <lane.s@unimelb.edu.au>
2017-08-21 11:56:27 +1000

    indentation

--- packages-theming.el
+++ packages-theming.el
@@ -1,9 +1,9 @@
-;; Time-stamp: <2017-08-21 11:04:43 (slane)>
+;; Time-stamp: <2017-08-21 11:56:21 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; Theming
 (use-package dracula
-	     :ensure t
-	     :init
-	     (load-theme 'dracula t)
-	     )
+  :ensure t
+  :init
+  (load-theme 'dracula t)
+  )
