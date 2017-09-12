05016da84af6cdd960e34cd2bf1699fc76b83e2f
Steve Lane <lane.s@unimelb.edu.au>
2017-09-12 14:19:08 +1000

    switch theme

--- packages-theming.el
+++ packages-theming.el
@@ -1,9 +1,31 @@
-;; Time-stamp: <2017-08-21 13:10:45 (slane)>
+;; Time-stamp: <2017-09-12 14:18:19 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; Theming
-(use-package dracula-theme
+;; (use-package dracula-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'dracula t)
+;;   )
+
+;; Try a couple of others
+;; (use-package noctilux-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'noctilux t)
+;;   )
+(use-package atom-one-dark-theme
   :ensure t
   :init
-  (load-theme 'dracula t)
+  (load-theme 'atom-one-dark t)
   )
+;; (use-package monokai-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'monokai t)
+;;   )
+;; (use-package zenburn-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'zenburn t)
+;;   )
