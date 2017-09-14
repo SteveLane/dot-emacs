3b85977e2429a29fafb477366f76592dfe87160c
Steve Lane <lane.s@unimelb.edu.au>
2017-09-14 10:02:10 +1000

    monokai theme
    monokai seems to be the only theme to work well both in terminal and
    windowed mode.

--- packages-theming.el
+++ packages-theming.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2017-09-12 14:18:19 (slane)>
+;; Time-stamp: <2017-09-14 10:01:32 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; Theming
@@ -14,18 +14,23 @@
 ;;   :init
 ;;   (load-theme 'noctilux t)
 ;;   )
-(use-package atom-one-dark-theme
-  :ensure t
-  :init
-  (load-theme 'atom-one-dark t)
-  )
-;; (use-package monokai-theme
+;; (use-package atom-one-dark-theme
 ;;   :ensure t
 ;;   :init
-;;   (load-theme 'monokai t)
+;;   (load-theme 'atom-one-dark t)
 ;;   )
+(use-package monokai-theme
+  :ensure t
+  :init
+  (load-theme 'monokai t)
+  )
 ;; (use-package zenburn-theme
 ;;   :ensure t
 ;;   :init
 ;;   (load-theme 'zenburn t)
+;;   )
+;; (use-package ubuntu-theme
+;;   :ensure t
+;;   :init
+;;   (load-theme 'ubuntu t)
 ;;   )
