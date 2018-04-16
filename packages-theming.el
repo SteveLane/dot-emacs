0682d6a12e48516b53e71918e8ea67ad2b583c75
Steve Lane <lane.s@unimelb.edu.au>
2018-04-16 11:23:31 +1000

    ensure spacemacs loads correctly

--- packages-theming.el
+++ packages-theming.el
@@ -1,19 +1,22 @@
-;; Time-stamp: <2017-09-14 13:48:19 (slane)>
+;; Time-stamp: <2018-04-16 11:23:22 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; Theming
 ;; (use-package dracula-theme
 ;;   :ensure t
-;;   :init
-;;   (load-theme 'dracula t)
+;;   :defer t
+;;   :init (progn (load-theme 'dracula t)
+;; 	       (enable-theme 'dracula))
 ;;   )
 ;; (use-package monokai-theme
 ;;   :ensure t
-;;   :init
-;;   (load-theme 'monokai t)
+;;   :defer t
+;;   :init (progn (load-theme 'monokai t)
+;; 	       (enable-theme 'monokai))
 ;;   )
 (use-package spacemacs-theme
   :ensure t
-  :init
-  (load-theme 'spacemacs-dark t)
+  :defer t
+  :init (progn (load-theme 'spacemacs-dark t)
+	       (enable-theme 'spacemacs-dark))
   )
