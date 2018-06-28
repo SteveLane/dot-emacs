01bfc871dc7fae18dda8927d36f63027af821f5b
Steve Lane <lane.s@unimelb.edu.au>
2018-06-28 20:19:06 +1000

    switch theme

--- packages-theming.el
+++ packages-theming.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-06-27 20:34:06 (slane)>
+;; Time-stamp: <2018-06-28 15:30:05 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; Theming
@@ -20,9 +20,26 @@
 ;;   :init (progn (load-theme 'spacemacs-dark t)
 ;; 	       (enable-theme 'spacemacs-dark))
 ;;   )
-(use-package anti-zenburn-theme
-  :ensure t
-  :defer t
-  :init (progn (load-theme 'anti-zenburn t)
-	       (enable-theme 'anti-zenburn))
-  )
+;; (use-package anti-zenburn-theme
+;;   :ensure t
+;;   :defer t
+;;   :init (progn (load-theme 'anti-zenburn t)
+;; 	       (enable-theme 'anti-zenburn))
+;;   )
+;; (use-package heroku-theme
+;;   :ensure t
+;;   :defer t
+;;   :init (progn (load-theme 'heroku t)
+;; 	       (enable-theme 'heroku))
+;;   )
+;; (use-package ample-theme
+;;   :defer t
+;;   :ensure t
+;;   :init (progn (load-theme 'ample t t)
+;;                (load-theme 'ample-flat t t)
+;;                (load-theme 'ample-light t t)
+;;                (enable-theme 'ample-light))
+;;   )
+;; Use the  built-in dichromacy
+(load-theme 'dichromacy t)
+(enable-theme 'dichromacy)
