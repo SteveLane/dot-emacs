c43e00002a6a788377d3e7a2cb6a29dfef35bae7
Steve Lane <lane.s@unimelb.edu.au>
2018-07-06 12:16:41 +1000

    try and work out framing for fonts
    probs still not working...

--- fonts.el
+++ fonts.el
@@ -6,3 +6,9 @@
 (when (member "Iosevka" (font-family-list))
   (set-face-attribute 'default nil :font "Iosevka"
 		      :weight 'light :height 200))
+
+;; (as above, but when starting a new frame, e.g. when using daemon)
+(when (member "Iosevka" (font-family-list))
+  (add-to-list 'default-frame-alist '(font . "-*-Iosevka-light-normal-normal-*-20-*-*-*-m-0-iso10646-1"))
+  (set-face-attribute 'default (selected-frame) :font "-*-Iosevka-light-normal-normal-*-20-*-*-*-m-0-iso10646-1")
+  )
