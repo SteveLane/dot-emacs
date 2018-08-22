dc7a84ec160ab848c131fadaf2b83ff99d3cc892
Steve Lane <lane.s@unimelb.edu.au>
2018-08-23 09:22:56 +1000

    defaults for stan

--- packages-stan.el
+++ packages-stan.el
@@ -1,8 +1,12 @@
-;; Time-stamp: <2017-08-21 11:43:58 (slane)>
+;; Time-stamp: <2018-08-21 10:18:54 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; stan-mode
 (use-package stan-mode
   :ensure t
   :mode ("\\.stan\\'" . stan-mode)
+  )
+
+(use-package stan-snippets
+  :ensure t
   )
