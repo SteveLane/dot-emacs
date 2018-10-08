a1f9c86003c1b7c0cdb4afc1060e2a783369de7f
Steve Lane <lane.s@unimelb.edu.au>
2018-10-09 10:10:13 +1100

    split out polymodes
    but they're still not playing well with yasnippet. fuck.

--- packages-polymode.el
+++ packages-polymode.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-10-09 09:24:04 (slane)>
+;; Time-stamp: <2018-10-09 09:52:23 (slane)>
 ;; Split out package loading into a separate file.
 ;; Code highlighting via polymode
 (use-package markdown-mode
@@ -15,9 +15,6 @@
   :ensure markdown-mode
   :ensure poly-R
   :ensure poly-noweb
-  :init
-  (require 'poly-R)
-  (require 'poly-noweb)
   :config
   ;; R/tex polymodes
   (add-to-list 'auto-mode-alist '("\\.Rnw" . poly-noweb+r-mode))
@@ -26,7 +23,7 @@
   ;; org-mode poly (not working at the moment)
   ;; (add-to-list 'auto-mode-alist '("\\.org" . poly-org-mode))
   ;; Make sure r-mode is loaded
-  (autoload 'r-mode "ess-site.el" "Major mode for editing R source." t)
+  ;; (autoload 'r-mode "ess-site.el" "Major mode for editing R source." t)
 
   ;; Add a chunk for rmarkdown
   ;; Need to add a keyboard shortcut
@@ -48,6 +45,24 @@
   ;; Flyspell on
   (add-hook 'markdown-mode-hook (lambda () (flyspell-mode 1)))
   )
+
+;; poly-R
+(use-package poly-R
+  :ensure polymode
+  :ensure poly-markdown
+  :ensure poly-noweb
+  :defer t
+  :config
+  ;; Add a chunk for rmarkdown
+  ;; Need to add a keyboard shortcut
+  ;; https://emacs.stackexchange.com/questions/27405/insert-code-chunk-in-r-markdown-with-yasnippet-and-polymode
+  ;; (defun insert-r-chunk (header) 
+  ;;   "Insert an r-chunk in markdown mode. Necessary due to interactions between polymode and yas snippet" 
+  ;;   (interactive "sHeader: ") 
+  ;;   (insert (concat "```{r " header "}\n\n\n```")) 
+  ;;   (forward-line -2))
+  ;; (define-key poly-markdown+r-mode-map (kbd "M-c") #'insert-r-chunk)
+)
 
 ;; Add yaml to markdown an .yml files
 (use-package yaml-mode
