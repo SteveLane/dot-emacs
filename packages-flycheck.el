b70e4e48d992d45056f441216734e2188c1ee1f4
Steve Lane <lane.s@unimelb.edu.au>
2019-01-14 14:13:12 +1100

    change defaults for flycheck

--- packages-flycheck.el
+++ packages-flycheck.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2018-10-09 10:06:21 (slane)>
+;; Time-stamp: <2018-12-13 12:16:56 (slane)>
 ;; Split out package loading into a separate file.
 ;; Now using use-package
 ;; flycheck
@@ -10,4 +10,13 @@
   (add-hook 'ess-mode-hook (lambda () (flycheck-mode t)))
   (add-hook 'LaTeX-mode-hook (lambda () (flycheck-mode t)))
   (add-hook 'sh-mode-hook (lambda () (flycheck-mode t)))
+
+  ;; Change defaults...
+  ;; Only check on save, or if idle for longer than 5 seconds.
+  (setq flycheck-check-syntax-automatically
+	'(save
+	  idle-change
+	  mode-enabled)
+	)
+  (setq flycheck-idle-change-delay 5.0)
   )
