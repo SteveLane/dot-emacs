b52a5cbade31ed7cd5a47f6773cef0e7489cc153
Steve Lane <lane.s@unimelb.edu.au>
2019-03-28 09:49:30 +1100

    add in ess configs

--- ess-config.el
+++ ess-config.el
@@ -1,25 +1,10 @@
-;; Time-stamp: <2018-12-03 14:34:42 (slane)>
-;; Split out package loading into a separate file.
-;; ESS
-(use-package ess
-  ;; installs if not already installed
-  :ensure t
-  :ensure julia-mode
-  :init (require 'ess-site)
-  :diminish eldoc-mode
-  :defer 1
-  ;; add jags mode (others taken care of)
-  :mode ("\\.[Jj][Aa][Gg]\\'" . ess-jags-mode)
-  ;; remap some keys
-  :bind
-  (:map ess-mode-map
-        (";" . ess-insert-assign))
-  (:map inferior-ess-mode-map
-        (";" . ess-insert-assign))
-  (:map ess-mode-map
-	("M-p" . #'my/add-pipe))
+;; Time-stamp: <2019-03-28 09:16:05 (slane)>
+;; Extra config for ESS that's required as spacemacs has some weird defaults.
+(with-eval-after-load 'ess-mode
+  (define-key ess-mode-map ";" 'ess-insert-assign)
+  (define-key inferior-ess-mode-map ";" 'ess-insert-assign)
+  (define-key ess-mode-map "M-p" #'my/add-pipe)
   ;; Set ESS up the way you like it
-  :config
   (setq-default inferior-R-args "--no-restore-history --no-restore --no-save")
   (add-hook 'ess-mode-hook (lambda () (auto-fill-mode 1)))
   (setq ess-ask-for-ess-directory nil)
@@ -34,10 +19,8 @@
   ;; Remove old _ mapping
   (setq ess-smart-S-assign-key nil)
   ;; Similarly for bugs/jags mode
-  (define-key ess-jags-mode-map "_" nil)
-  (define-key ess-jags-mode-map ";" #'ess-bugs-hot-arrow)
-  
-  )
+  ;; (define-key ess-jags-mode-map "_" nil)
+  ;; (define-key ess-jags-mode-map ";" #'ess-bugs-hot-arrow)
 
 ;; Function to add the pipe operator (set in map above)
 (defun my/add-pipe ()
@@ -46,3 +29,4 @@
   (just-one-space 1)
   (insert "%>%")
   (ess-newline-and-indent))
+)
