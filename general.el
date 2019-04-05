77b6b24d4ec35c1f10157f55e723a06ff5ac50cb
Steve Lane <lane.s@unimelb.edu.au>
2019-04-05 12:25:06 +1100

    the general configuration stuff
    the config in here is not application specific

---
+++ general.el
@@ -0,0 +1,42 @@
+;; This is a general settings file for top-level settings
+
+;; Where to save abbrevs (and silently)
+(setq abbrev-file-name "~/.emacs.d/.abbrev_defs")
+(setq save-abbrevs 'silently)
+
+;; Here I'm changing my option and meta keys to where I would normally have them.
+(setq mac-option-key-is-meta nil
+	    mac-command-key-is-meta t
+	    mac-command-modifier 'meta
+	    mac-option-modifier 'none)
+
+;; OK, now I'm going to remap escaping (are you jk'ing? haha)
+(setq-default
+ evil-escape-key-sequence "jk"
+ evil-escape-unordered-key-sequence "true"
+)
+
+;; I'm sick of backups... at least in my working directory.
+;; Let's place them somewhere else (plus some other cool stuff:
+(setq
+ backup-by-copying t      ; don't clobber symlinks
+ backup-directory-alist
+ '(("." . "~/.saves"))    ; don't litter my fs tree
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t)       ; use versioned backups
+
+;; Add a timestamp to files (and on save)
+;; http://emacs-fu.blogspot.com.au/2008/12/automatic-timestamps.html
+(setq 
+ time-stamp-active t          ; do enable time-stamps
+ time-stamp-line-limit 20     ; check first 10 buffer lines for Time-stamp: 
+ time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S (%u)") ; date format
+(add-hook 'write-file-hooks 'time-stamp) ; update when saving
+
+;; Make sure that [mM]akefile's with an 'extension' are opened in makefile-mode
+(add-to-list 'auto-mode-alist '("[mM]akefile\\.[a-zA-Z]*\\'" . makefile-mode))
+
+;; make spacemacs the global git editor
+(global-git-commit-mode t)
