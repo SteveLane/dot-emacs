34d19fe200ff939aaf0ddc382db04fbde38a50a1
Steve Lane <lane.s@unimelb.edu.au>
2019-04-18 10:29:22 +1000

    fix pipe

--- ess-config.el
+++ ess-config.el
@@ -1,9 +1,8 @@
-;; Time-stamp: <2019-04-17 14:37:34 (slane)>
+;; Time-stamp: <2019-04-17 15:07:45 (slane)>
 ;; Extra config for ESS that's required as spacemacs has some weird defaults.
 (with-eval-after-load 'ess-mode
   (define-key ess-mode-map ";" 'ess-insert-assign)
   (define-key inferior-ess-mode-map ";" 'ess-insert-assign)
-  (define-key ess-mode-map "M-p" #'my/add-pipe)
   ;; Set ESS up the way you like it
   (setq-default inferior-R-args "--no-restore-history --no-restore --no-save")
   (add-hook 'ess-mode-hook (lambda () (auto-fill-mode 1)))
@@ -30,12 +29,14 @@
   ;; Similarly for bugs/jags mode
   ;; (define-key ess-jags-mode-map "_" nil)
   ;; (define-key ess-jags-mode-map ";" #'ess-bugs-hot-arrow)
-
-;; Function to add the pipe operator (set in map above)
-(defun my/add-pipe ()
-  "Adds a pipe operator %>% with one space to the left and then starts a newline with proper indentation"
-  (interactive)
-  (just-one-space 1)
-  (insert "%>%")
-  (ess-newline-and-indent))
+  
+  ;; Function to add the pipe operator (set in map above)
+  (defun my-add-pipe ()
+    "Adds a pipe operator %>% with one space to the left and then starts a newline with proper indentation"
+    (interactive)
+    (just-one-space 1)
+    (insert "%>%")
+    (reindent-then-newline-and-indent)
+    )
+  (define-key ess-mode-map (kbd "M-p") 'my-add-pipe)
 )
