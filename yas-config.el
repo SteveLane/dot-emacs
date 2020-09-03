fd343ce1ce2fae6b0169607e3016c20d58aabc74
Steve Lane <me@somewhere>
2020-09-04 09:38:55 +1000

    yas config updated

--- yas-config.el
+++ yas-config.el
@@ -1,10 +1,10 @@
-;; Time-stamp: <2019-04-05 13:46:42 (slane)>
+;; Time-stamp: <2020-09-03 08:50:05 (lanes1)>
 ;; yasnippet
 ;; Many ideas from https://github.com/fniessen/emacs-leuven/blob/master/emacs-leuven.el
 (with-eval-after-load 'yasnippet
   (setq yas-snippet-dirs '("~/github/emacs-config/snippets")
         ;; Fix indentation
-        (setq yas-indent-line 'fixed)
+        yas-indent-line 'fixed
         )
   ;; Load snippet tables
   (yas-reload-all)
@@ -18,6 +18,9 @@
   (add-hook 'ess-jags-mode-hook 'yas/minor-mode-on)
   ;; Make sure yasnippet is on for rmarkdown files
   (add-hook 'markdown-mode-hook 'yas/minor-mode-on)
+  ;; Add snippet availability in all modes
+  (add-hook 'yas-minor-mode-hook (lambda ()
+                                   (yas-activate-extra-mode 'fundamental-mode)))
   ;; Don't expand when you are typing in a string or comment.
   (add-hook 'prog-mode-hook
 	    (lambda ()
