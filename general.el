c6722bc0073a9054bb09d709f42a0645cc0cce6c
Steve Lane <steve_lane@worksafe.vic.gov.au>
2021-04-07 19:44:26 +1000

    fixes to date stamp

--- general.el
+++ general.el
@@ -31,7 +31,7 @@
 (setq 
  time-stamp-active t          ; do enable time-stamps
  time-stamp-line-limit 20     ; check first 10 buffer lines for Time-stamp: 
- time-stamp-format "%04y-%02m-%02d %02H:%02M:%02S (%u)") ; date format
+ time-stamp-format "%Y-%m-%d %02H:%02M:%02S (%u)") ; date format
 (add-hook 'write-file-hooks 'time-stamp) ; update when saving
 
 ;; Make sure that [mM]akefile's with an 'extension' are opened in makefile-mode
