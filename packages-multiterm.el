5a733c0e39d178c8854a35ec88cc4cd3e89e3f63
Steve Lane <sprazza@Steves-Air.lan>
2021-05-03 15:47:29 +1000

    remove unwanted packages files

--- packages-multiterm.el
+++
@@ -1,9 +0,0 @@
-;; Time-stamp: <2017-08-28 11:13:04 (slane)>
-;; Split out package loading into a separate file.
-;; Now using use-package
-;; multi-term
-(use-package multi-term
-  :ensure t
-  :config
-  (setq multi-term-program "/bin/bash")
-  )
