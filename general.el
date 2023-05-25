bdfbdbb8bfe55c80c916d738f2a82069c51fabd2
lanes1 <steve_lane@worksafe.vic.gov.au>
2023-05-26 08:51:55 +1000

    when on windows, use unix tools if already built and available

--- general.el
+++ general.el
@@ -74,3 +74,17 @@
 
 ;; Set environment variable for display of unicode in ipython
 (setenv "PYTHONIOENCODING" "utf-8")
+
+;; Also on windows, make sure to use bash versions of commands
+(when (eq system-type 'windows-nt)
+
+  ;; Make sure Unix tools are in front of `exec-path'
+  (let ((bash (executable-find "bash")))
+    (when bash
+      (push (file-name-directory bash) exec-path)))
+
+  ;; Update PATH from exec-path
+  (let ((path (mapcar 'file-truename
+                      (append exec-path
+                              (split-string (getenv "PATH") path-separator t)))))
+    (setenv "PATH" (mapconcat 'identity (delete-dups path) path-separator))))
